84b4d8adc9e2fe77213fd7fb3f472eda62da5430
Dawid <skiperpol1@gmail.com>
2026-04-22 17:31:36 +0200

    feat: add SearchComponent for enhanced search functionality
    - Introduced a new SearchComponent to streamline search input with an integrated icon.
    - Updated app.json to include last update timestamp for better version tracking.

---
+++ src/components/SearchComponent.tsx
@@ -0,0 +1,27 @@
+import { Search } from "lucide-react-native";
+import { View } from "react-native";
+import { Input } from "./ui/input";
+
+export function SearchComponent({
+  placeholder,
+  value,
+  onChangeText,
+}: {
+  placeholder: string;
+  value: string;
+  onChangeText: (text: string) => void;
+}) {
+  return (
+    <View className="mb-3 relative justify-center">
+      <View className="absolute left-3 z-10">
+        <Search size={18} className="text-foreground" />
+      </View>
+      <Input
+        placeholder={placeholder}
+        className="h-12 pl-10 placeholder:text-foreground"
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+}
